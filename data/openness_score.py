53efec774ec912e13a1234dd3e012e0917eea3fd
patiphan2000 <srichai.patiphan@gmail.com>
2026-08-06 21:59:18 +0700

    feat(data): normalize both respondent and country df

--- data/openness_score.py
+++ data/openness_score.py
@@ -14,7 +14,7 @@ def transpose_df(df: pd.DataFrame | None) -> pd.DataFrame | None:
     return df_t.reset_index(drop=True)
 
 
-def normalize_respondent_info(df: pd.DataFrame | None):
+def normalize_info_df(df: pd.DataFrame | None):
     _df = transpose_df(df)
     if _df is None:
         return
@@ -42,8 +42,8 @@ class OpennessScore:
         self.upper_chamber_df = upper_chamber_df
 
         # Normalize respondent_info_df & country_context_df
-        self.respondent_info_df = normalize_respondent_info(respondent_info_df)
-        self.country_context_df = transpose_df(country_context_df)
+        self.respondent_info_df = normalize_info_df(respondent_info_df)
+        self.country_context_df = normalize_info_df(country_context_df)
 
     def get_country_info(self) -> pd.DataFrame:
         return pd.concat([self.respondent_info_df, self.country_context_df], axis=1)
