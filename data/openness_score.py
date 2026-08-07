66800b2391c8167764253a03e1c3b5da44eb2ef0
patiphan2000 <srichai.patiphan@gmail.com>
2026-08-07 10:07:48 +0700

    refactor(data): move utility function to utilities.py

--- data/openness_score.py
+++ data/openness_score.py
@@ -1,38 +1,11 @@
-import re
 import pandas as pd
+from utilities import normalize_info_df
 from constants import (
     COUNTRIES_DEFAULT_COLUMNS,
     RESPONDENTS_DEFAULT_COLUMNS,
     INDICATORS_DEFAULT_COLUMNS,
     INDICATORS_TRANSFORM_COLUMNS,
 )
-
-
-def transpose_df(df: pd.DataFrame | None) -> pd.DataFrame | None:
-    if df is None:
-        return
-
-    # Set the first column as index and transpose
-    df_t = df.set_index(df.columns[0]).T
-
-    # Clean up index/column names
-    df_t.columns.name = None
-    return df_t.reset_index(drop=True)
-
-
-def normalize_info_df(df: pd.DataFrame | None):
-    _df = transpose_df(df)
-    if _df is None:
-        return
-
-    # Clean character prefix from respondent_info_df's columns
-    old_columns = _df.columns
-    _df = _df.rename(
-        columns={
-            col: re.sub(r"^[a-z]{1}\.\s+|\:$", "", col).strip() for col in old_columns
-        }
-    )
-    return _df
 
 
 class OpennessScore:
