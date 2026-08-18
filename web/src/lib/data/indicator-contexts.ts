4ca75775e56e486be06c7adb52796e46c4126e23
Th1nkK1D <witheep@gmail.com>
2026-08-18 11:21:13 +0700

    chore(web): update sheethuahua to 4.0.1 with newline list support

--- web/src/lib/data/indicator-contexts.ts
+++ web/src/lib/data/indicator-contexts.ts
@@ -1,31 +1,22 @@
 import indicatorContextsCsv from '$data/indicator-contexts.csv?raw';
 import {
+	asArray,
 	asNumber,
 	asOneOf,
 	asString,
 	Column,
-	createTransformer,
 	Object,
 	parseCsv,
 	type StaticDecode
 } from 'sheethuahua';
 import { chambers } from '$lib/constants/chambers';
 
-/** Splits newline-separated links, `asArray` can't be used since it treats newline as a row break */
-const asLines = createTransformer({
-	decode: (value: string) =>
-		value
-			.split('\n')
-			.map((line) => line.trim())
-			.filter((line) => line.length > 0)
-});
-
 export const indicatorContextSchema = Object({
 	country: Column('Country', asString()),
 	chamber: Column('Chamber', asOneOf(chambers)),
 	indicatorNumber: Column('Indicator Number', asNumber()),
 	context: Column('Context', asString().optional()),
-	evidences: Column('Evidences', asLines.optional([]))
+	evidences: Column('Evidences', asArray(asString(), '\n').optional([]))
 });
 
 export type IndicatorContext = StaticDecode<typeof indicatorContextSchema>;
